9fde3c1f13a5181cae6c066d763b40a89b65cdd2
Stefan Wilfinger <bef@gmx.net>
2025-02-08 10:06:09 +0100

    return error on IsMoveValid introduce MustMakeMove add and refactor tests add TODOs

--- game/move.go
+++ game/move.go
@@ -2,11 +2,13 @@ package game
 
 type Move struct {
 	Piece Piece
-	From *Position
-	To Position
+	From  *Position
+	To    Position
 }
 
 type Position struct {
 	Row int
 	Col int
 }
+
+// TODO implement 2 Move constructors (for placing and moving a piece)
